34403a835d4c3d25b08513c2c54fd61065fcc0e9
domenicog03 <domenico.galeone.info@gmail.com>
2024-03-07 15:51:41 +0100

    feat: stampa fizz, buzz e FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 
-for (let i=0; i<=100; i++){
-    console.log(i);
+for (let i=1; i<=100; i++){
+
+    if ( (i % 3 == 0) && (i % 5 == 0)){
+        console.log('FizzBizz');
+    } else if(i % 3 ==0 ){
+        console.log('Fizz');
+    } else if ( i % 5 == 0){
+        console.log('Buzz');
+    }  else {
+        console.log(i);
+    }
+    
 }
