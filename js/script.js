57e63eb6ea6d464a96a0d2247c453b093e56bbb4
domenicog03 <domenico.galeone.info@gmail.com>
2024-03-07 16:07:46 +0100

    feat: correzzione parola FizzBuzz

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 for (let i=1; i<=100; i++){
 
     if ( (i % 3 == 0) && (i % 5 == 0)){
-        console.log('FizzBizz');
+        console.log('FizzBuzz');
     } else if(i % 3 ==0 ){
         console.log('Fizz');
     } else if ( i % 5 == 0){
